398a9073148cdb162d627e0391faeb5aaf545574
Syed Hussain Mehdi <syed@mufi.app>
2023-04-18 01:55:07 +0500

    simple ws

---
+++ lru_cache.js
@@ -0,0 +1,44 @@
+const { LRUCache } = require('lru-cache')
+
+const options = {
+    max: 500,
+
+    // for use with tracking overall storage size
+    maxSize: 5000,
+    sizeCalculation: (value, key) => {
+        return 1
+    },
+
+    // for use when you need to clean up something when objects
+    // are evicted from the cache
+    dispose: (value, key) => {
+        freeFromMemoryOrWhatever(value)
+    },
+
+    // how long to live in ms
+    ttl: 1000 * 60 * 5,
+
+    // return stale items before removing from cache?
+    allowStale: true,
+
+    updateAgeOnGet: true,
+    updateAgeOnHas: true,
+
+    // async method to use for cache.fetch(), for
+    // stale-while-revalidate type of behavior
+    fetchMethod: async (
+        key,
+        staleValue,
+        { options, signal, context }
+    ) => { },
+}
+
+const buildCache = () => {
+    const cache = new LRUCache(options)
+    return cache
+}
+
+module.exports = {
+    buildCache,
+    options
+}   
--- server.js
+++ server.js
@@ -2,9 +2,18 @@
 
 const express = require('express');
 const { Server } = require('ws');
+const axios = require('axios');
+const { buildCache } = require('./lru_cache');
+
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+const DISCOG_API = 'https://dgr4q70dil.execute-api.us-east-1.amazonaws.com/prod/catalog?id='
+
+
+const discogCache = buildCache();
+const pplCache = buildCache();
+
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -15,10 +24,61 @@ const wss = new Server({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', async (message) => {
+    try {
+      const payload = JSON.parse(message);
+      if (payload.key === 'processArtist') {
+        const artistId = payload.value;
+        if (discogCache.has(artistId)) {
+          console.log('discog data already in cache')
+        } else {
+          // fetch artist data from discogs
+          const response = await axios.get(DISCOG_API + artistId)
+          const discog = response.data;
+          discogCache.set(artistId, discog);
+          console.log('discog data fetched successfully')
+        }
+        if (pplCache.has(artistId)) {
+          console.log('ppl data already in cache')
+        } else {
+          // fetch artist data from ppl
+          wss.clients.forEach((client) => {
+            client.send(JSON.stringify({
+              key: 'pplScrape',
+              value: artistId
+            }));
+          });
+        }
+      }
+      else if (payload.key === 'pplScrapeResult') {
+        const artistId = payload.artistId;
+        const ppl = payload.value;
+        pplCache.set(artistId, ppl);
+        console.log('ppl data fetched successfully')
+      }
+      else if (payload.key === 'getArtist') {
+        const artistId = payload.value;
+        const discog = discogCache.get(artistId);
+        const ppl = pplCache.get(artistId);
+        ws.send(JSON.stringify({
+          key: 'getArtistResult',
+          value: {
+            discog,
+            ppl
+          }
+        }));
+      }
+    } catch (e) {
+      console.log('Invalid JSON');
+      return;
+    }
+  });
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+
+
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
